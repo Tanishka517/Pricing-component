ee88b76ff36a9ca6af85c20cf44157b2c9f825b9
techfantictanishka <your.email@example.com>
2026-05-26 19:54:57 +0530

    Add JavaScript toggle functionality

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-const toggle = document.getElementbyID("billingToggle");
-const basic_price = document.getElementbyID("basic-price");
-const pro_price = document.getElementbyID("pro-price");
-const master_price = document.getElementbyID("master-price");
+const toggle = document.getElementById("billingToggle");
+const basic_price = document.getElementById("basic-price");
+const pro_price = document.getElementById("pro-price");
+const master_price = document.getElementById("master-price");
 
 toggle.addEventListener("change", () => {
-if(toggle.checked){
-
-basic_price.textContent="$199.99 ";
-pro_price.textContent="$249";
-master_price.textContent="$399";
-    }
-
-    else{
-basic_price.textContent="$19.99";
-pro_price.textContent="$29.99";
-master_price.textContent="$39.99";
-
-    }
-
-
-
-
-}
-)
+  if (toggle.checked) {
+    basic_price.textContent = "$199.99";
+    pro_price.textContent = "$249.99";
+    master_price.textContent = "$399.99";
+  } else {
+    basic_price.textContent = "$19.99";
+    pro_price.textContent = "$29.99";
+    master_price.textContent = "$39.99";
+  }
+});
